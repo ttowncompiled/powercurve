5a47cb60eadbe8c28dafa233bcf547149658fd9b
Ian Riley <ian.riley.915@gmail.com>
2016-01-11 00:16:10 -0600

    Finished combat.

--- app/lib/simulation.js
+++ app/lib/simulation.js
@@ -1,5 +1,91 @@
+function RollDie(die) {
+  return Math.floor((20 * Math.random()) + 1);
+}
+
+function MakeSavingThrow(die, save, check) {
+  var roll = RollDie(die);
+  if (roll == 1) return false;
+  if (roll == 20) return true;
+  return roll + save > check;
+}
+
+function ApplyDamage(attacker, defender, roll, isCrit) {
+  // damage vs aura vs vitality
+  var roll = RollDie(attacker['dmg']['die']);
+  var damage = attacker['dmg']['mult'] * roll + attacker['dmg']['mod'];
+  if (damage > defender['ardx']) {
+    defender['vit'] -= (damage - defender['ardx']);
+    damage = defender['ardx'];
+  }
+  if (damage > defender['aura']) {
+    defender['aura'] = 0;
+    defender['vit'] -= (damage - defender['aura']);
+    return;
+  }
+  defender['aura'] -= damage;
+}
+
+function CheckMeleeAttack(attacker, defender) {
+  var roll = RollDie(attacker['atk']['die']);
+  if (roll == 1) return;
+  if (roll >= 20 - attacker['crit']) ApplyDamage(attacker, defender, roll, true);
+  // attack vs defense
+  if (attacker['atk']['mult'] * roll + attacker['atk']['mod'] > defender['def']) {
+    ApplyDamage(attacker, defender, roll, false);
+  }
+}
+
+function CheckRangedAttack(attacker, defender) {
+  var roll = RollDie(attacker['atk']['die']);
+  if (roll == 1) return;
+  if (roll >= 20 - attacker['crit']) ApplyDamage(attacker, defender, roll, true);
+  // attack vs reflex vs defense
+  if (!MakeSavingThrow(20, defender['rflx'], 40)) {
+    if (attacker['atk']['mult'] * roll + attacker['atk']['mod'] > defender['def']) {
+      ApplyDamage(attacker, defender, roll, false);
+    }
+  }
+}
+
+function RollForInitiative() {
+  return RollDie(20) >= RollDie(20);
+}
+
+function Attack(attacker, defender) {
+  if (attacker['form'] == 'melee') CheckMeleeAttack(attacker, defender);
+  if (attacker['form'] == 'ranged') CheckRangedAttack(attacker, defender);
+  // return true if the defender's vitality was reduced to 0 by the attack
+  return defender['vit'] <= 0;
+}
+
 function BasicCombat(player, opp) {
-  return true;
+  // choose attack order
+  var first;
+  var second;
+  if (RollForInitiative()) {
+    first = player;
+    second = opp;
+  } else {
+    first = opp;
+    second = player;
+  }
+  // rounds last until one character's vitality is reduced to 0
+  while (!Attack(first, second) && !Attack(second, first)) {
+    // aura is regenerated at the end of each round
+    if (first['aura'] > 0) first['aura'] += first['argn'];
+    if (second['aura'] > 0) second['aura'] += second['argn'];
+  }
+  // return true if the player has won
+  return player['vit'] > 0;
+}
+
+function ComputeDieTemplate(template) {
+  var parts = template.split(' ');
+  return {
+    mult: parts[0],
+    die: parts[2],
+    mod: parts[4]
+  };
 }
 
 function ComputeStatTemplate(template, builder, level) {
