3bf87feea6546bda521e08b9f184b5314fd2ed1a
Ian Riley <ian.riley.915@gmail.com>
2016-01-10 23:06:41 -0600

    Adds base line calculations.

--- app/lib/simulation.js
+++ app/lib/simulation.js
@@ -1,25 +1,43 @@
-function Annotate(data, labels, params) {
-  var annotated_data = [];
-  for (var i = 0; i < data.length; i++) {
-    if (data[i].length != params['lvl-under'] + params['lvl-over'] + 1) {
-      return [];
+function BasicCombat(player, opp) {
+  return true;
+}
+
+function BuildCharacter(character, level) {
+  return {}
+}
+
+function BaseLine(character, params) {
+  var baseLine = []
+  for (var x = -1 * params['lvl-under']; x <= params['lvl-over']; x++) {
+    var wins = 0;
+    for (var i = 0; i < params['iters']; i++) {
+      var dynamic = BuildCharacter(character, params['lvl'] + x)
+      var static = BuildCharacter(character, params['lvl'])
+      if (BasicCombat(dynamic, static)) {
+        wins++;
+      }
     }
-    var results = [];
-    for (var j = 0; j < data[i].length; j++) {
-      results.push([-1 * params['lvl-under'] + j, data[i][j]]);
-    }
-    annotated_data.push({data: results, label: ''});
+    var y = wins / params['iters'];
+    baseLine.push([x, y]);
   }
-  return annotated_data;
+  return {data: baseLine, label: character['name'] + ' - Base'};
+}
+
+function CalculateBaseLines(player, opps, params) {
+  var results = {data: [], labels: []};
+  var pbl = BaseLine(player, params);
+  results['data'].push(pbl['data']);
+  results['labels'].push(pbl['label']);
+  opps.forEach(function(o) {
+    var obl = BaseLine(o, params);
+    results['data'].push(obl['data']);
+    results['labels'].push(obl['label']);
+  });
+  return results;
 }
 
 function Simulate(player, opps, params) {
-  var y = [];
-  for (var i = 0; i < params['lvl-under'] + params['lvl-over']; i++) {
-    y.push(0);
-  }
-  y.push(1);
-  return Annotate([y], [], params);
+  return CalculateBaseLines(player, opps, params);
 }
 
 onmessage = function(deps) {
